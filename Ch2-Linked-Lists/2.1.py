8186cfca5d72178773018abef76d26047f562136
caleb765landis <caleb765landis@gmail.com>
2023-09-05 18:10:41 -0600

    Updated LinkedList

--- Ch2-Linked-Lists/2.1.py
+++ Ch2-Linked-Lists/2.1.py
@@ -8,11 +8,33 @@ Q: pg.94
 A: pg.208
 """
 
-from LinkedList import LinkedList, testLinkedList
+from LinkedList import LinkedList, createLinkedList, testLinkedList
 
 def main():
-    testLinkedList
+    # testLinkedList
 
+    testTwoPointers()
+
+def testTwoPointers():
+    ll = createLinkedList([9, 8, 7, 6, 5, 4, 3, 2])
+    fastPtr = ll.head
+    slowPtr = ll.head
+
+    fastPtrIndex = 1
+
+    for i in range(0, ll.length):
+        print(slowPtr.dataVal)
+        print(fastPtr.dataVal)
+        print("")
+
+        slowPtr = slowPtr.next
+        fastPtrIndex += 2
+
+        if fastPtrIndex >= ll.length:
+            fastPtr = ll.head
+            fastPtrIndex = 1
+        else:
+            fastPtr = fastPtr.next.next
 
 if __name__ == "__main__":
     main()
--- Ch2-Linked-Lists/LinkedList.py
+++ Ch2-Linked-Lists/LinkedList.py
@@ -7,10 +7,15 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
 
+        self.length = 0
+
         if (self.head != None):
             current = self.head
+            self.length += 1
+
             while (current.next != None):
                 current = current.next
+                self.length += 1
 
             self.tail = current
         else:
@@ -29,6 +34,8 @@ class LinkedList:
             
             self.tail = end
 
+        self.length += 1
+
     def appendNodeToTail(self, node):
         if self.head == None:
             self.head = node
@@ -39,18 +46,52 @@ class LinkedList:
             node.prev = self.tail
             self.tail = node
 
-    def deleteNodeWithVal(self, val):
-        if (self.head == None): return
+        self.length += 1
 
-        current = self.head
+    # Deletes a node in the linked list
+    # Assumes node is a valid node that is part of the list
+    def deleteNode(self, node):
+        # if deleting head
+        if node == self.head:
+            # and if head has nodes after it
+            if node.next != None:
+                # set next node to be head
+                self.head = node.next
+                node.next.prev = None
+                self.length -= 1
+                return
+            # otherwise set head to none
+            else:
+                self.head = None
+                self.length -= 1
+                return
 
-        # while (current.next != None):
-        #     if 
+        # if deleting tail
+        if node == self.tail:
+            # and if tail has nodes before it
+            if node.prev != None:
+                # set the new tail to be current tail's previous node
+                self.tail = node.prev
+                node.prev.next = None
+                self.length -= 1
+                return
+            # otherwise set tail to None
+            else:
+                self.tail = None
+                self.length -= 1
+                return
 
-
-        #     current = current.next
+        # Node must not be head or tail,
+        # so set next and prev values of neighbor nodes
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        self.length -= 1
 
     def printVals(self):
+        if self.head == None:
+            print("No nodes in list.")
+            return
+
         current = self.head
 
         while (current.next != None):
@@ -58,6 +99,14 @@ class LinkedList:
             current = current.next
 
         print(current.dataVal)
+
+def createLinkedList(list):
+    ll = LinkedList()
+
+    for item in list:
+        ll.appendValToTail(item)
+
+    return ll
 
 def testLinkedList():
     ll1 = LinkedList()
@@ -70,6 +119,13 @@ def testLinkedList():
 
     ll1.appendValToTail(1)
     ll1.printVals()
+    print("Length: " + str(ll1.length))
+    print("")
+
+    ll1.deleteNode(ll1.head)
+    ll1.printVals()
+    print("Length: " + str(ll1.length))
+    print("")
 
     n1 = Node(1)
     ll2 = LinkedList(n1)
@@ -77,6 +133,41 @@ def testLinkedList():
     ll2.appendNodeToTail(n2)
     ll2.appendValToTail(3)
     ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    ll2.deleteNode(n2)
+    ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    ll2.deleteNode(ll2.tail)
+    ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    ll2.deleteNode(ll2.tail)
+    ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    n1 = Node(1)
+    ll2 = LinkedList(n1)
+    n2 = Node(2)
+    ll2.appendNodeToTail(n2)
+    ll2.appendValToTail(3)
+    ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    ll2.deleteNode(ll2.head)
+    ll2.printVals()
+    print("Length: " + str(ll2.length))
+    print("")
+
+    ll3 = createLinkedList([9, 8, 7, 6, 5, 4, 3])
+    ll3.printVals()
+    print("")
 
 if __name__ == '__main__':
     testLinkedList()
